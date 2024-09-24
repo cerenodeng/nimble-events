23009350018c2bf32051dc9cbfddf849c3d977c8
Cereno Deng <cereno.deng@outlook.com>
2024-09-25 00:15:51 +0800

    feat: update home page to display events info

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,3 +1,42 @@
-export default function Home() {
-	return <div className='text-4xl font-bold p-16'>Nimble Events</div>;
+import Strapi from 'strapi-sdk-js';
+
+export default async function Home() {
+  const strapi = new Strapi();
+  strapi.axios.defaults.headers.common['Authorization'] =
+    `Bearer ${process.env.STRAPI_API_TOKEN}`;
+  const { data: events } = await strapi.find('events', { populate: ['media'] });
+
+  return (
+    <div className='p-20'>
+      <h2 className='text-4xl font-bold'>Nimble Events</h2>
+      <div className='grid grid-cols-4 gap-10'>
+        {events.map((event) => (
+          <div className='card w-96 bg-base-100 shadow-xl' key={event.id}>
+            {/* {event.media && (
+            <div className='flex gap-x-10'>
+              {event.media.map((file) => (
+                <img
+                  src={`http://localhost:1337${file.formats.medium.url}`}
+                  alt={event.title}
+                  key={file.documentId}
+                />
+              ))}
+            </div>
+          )} */}
+            <figure>
+              {event.media && (
+                <img
+                  src={`http://localhost:1337${event.media[0].formats.medium.url}`}
+                  alt={event.title}
+                />
+              )}
+            </figure>
+            <div className='card-body'>
+              <h2 className='card-title'>{event.title}</h2>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
